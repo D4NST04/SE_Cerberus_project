2f58c96ecb34e6b78920f014e3057a0cf6fef612
mirai-ux <raaszard666@gmail.com>
2026-01-23 22:15:56 +0100

    fix(backend): hadfklij;hasfdjkl;adfgsjkl;adfgsjkl;adfgrjkl;

--- backend/src/image_processor.rs
+++ backend/src/image_processor.rs
@@ -1,6 +1,6 @@
 use image::io::Reader as ImageReader;
 use ndarray::Array4;
-use onnxruntime::{environment::Environment, session::SessionBuilder, tensor::OrtOwnedTensor};
+use onnxruntime::{environment::Environment, tensor::OrtOwnedTensor};
 use std::error::Error;
 
 fn load_image(path: &str) -> Result<image::RgbImage, Box<dyn Error>> {
@@ -28,13 +28,13 @@ pub fn face_embedding(image_path: &str, model_path: &str) -> Result<Vec<f32>, Bo
 
     let env = Environment::builder().with_name("face_embed").build()?;
 
-    let mut session = SessionBuilder::new(&env)?.with_model_from_file(model_path)?;
+    let mut session = env
+        .new_session_builder()?
+        .with_model_from_file(model_path)?;
 
-    let input_name = session.inputs[0].name.clone();
-    let outputs: Vec<OrtOwnedTensor<f32, _>> =
-        session.run(vec![(input_name.as_str(), input_tensor)])?;
+    let _input_name = session.inputs[0].name.clone();
+    let outputs: Vec<OrtOwnedTensor<f32, _>> = session.run(vec![input_tensor])?;
 
     let output = outputs[0].as_slice().unwrap().to_vec();
     Ok(output)
 }
-
