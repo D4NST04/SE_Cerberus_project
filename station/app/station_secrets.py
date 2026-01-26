c0235f784c6e901aacb9bbb881492674da096cb9
Daniel <daniel.stasielak+it@icloud.com>
2026-01-26 11:56:36 +0100

    additions after merging with backend

--- station/app/station_secrets.py
+++ station/app/station_secrets.py
@@ -1,6 +1,6 @@
 sweet_secrets = {
-    "qr_url" : "/employees/qr_check",
-    "face_url" : "/face/verify",
-    "ack_url" : "/access/ack",
+    "qr_url" : "http://backend:8080/api/employees/qr_check",
+    "face_url" : "http://backend:8080/api/face/verify",
+    "ack_url" : "http://backend:8080/api/access/ack",
     "auth_token" : None  # "Here put token if API needs it"
 }
